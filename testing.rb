7752643d003dc4bfc83ccaebdbcb23254008d981
rayoelperro <rayoelperro@gmail.com>
2019-08-07 12:32:29 +0200

    update

--- testing.rb
+++ testing.rb
@@ -3,15 +3,6 @@ require './database.rb'
 
 d = Kurt::DataBase.new("kurttesting",true)
 d.select("/main.json",true,"newtable",["name","surname","age"])
-=begin
-d.q_set("rayoelperro",{name:"alberto",surname:"elorza",age:30})
-d.q_del("rayoelperro",{AND:[["age",">",18]],OR:[["name","=","alberto"],["surname","=","elorza"]]})
-d.q_set("rayoelperro",{name:"pepe",surname:"elorza",age:20})
-d.q_set("rayoelperro",{name:"alberto",surname:"cardenas",age:19})
-d.q_set("rayoelperro",{name:"pepe",surname:"martinez",age:40})
-d.q_set("rayoelperro",{name:"arnaldo",surname:"navarro",age:40})
-puts d.q_get("rayoelperro",{AND:[["age",">",18]],OR:[["name","=","alberto"],["surname","=","elorza"]]})
-=end
 b = Kurt::QueryMaker.new(d)
 b.exec("SET newtable VALUES name : one surname : four age : 16")
 b.exec("SET newtable VALUES name : two surname : five age : 35")
